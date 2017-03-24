ce8c18e7d67cf0c8ca70b432481c2b0fcf03d97f
sandy17031995 <kumarsandeepranjan14@gmail.com>
2017-03-24 14:45:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,62 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone = {
+    title: 'article-one | sandy',
+    heading: 'this is article one',
+    date: '20 dec 2017',
+    content: `
+        <p>
+                hey guys, this is my first article.. im sandy.
+        </p>
+        <p>
+             A short story is a piece of prose fiction that can be read in one sitting. Emerging from earlier oral storytellin traditions in the 17th century, the short story has grown to encompass a body of work so diverse as to defy easy characterization. At its most prototypical the short story features a small cast of named characters, and focuses on a self-contained incident with the intent of evoking a "single effect" or mood.[1] In doing so, short stories make use of plot, resonance, and other dynamic components to a far greater degree than is typical of an anecdote, yet to a far lesser degree than a novel. While the short story is largely distinct from the novel, authors of both generally draw from a common pool of literary techniques.
+        </p>
+        <p>
+            sachin sandeep rahul.
+        </p>
+        <p>
+            The short story has been considered both an apprenticeship form preceding more lengthy works, and a crafted form in its own right, collected together in books of similar length, price, and distribution as novels. Short story writers may define their works as part of the artistic and personal expression of the form. They may also attempt to resist categorization by genre and fixed formation.
+        </p>`
+};
+
+function createtemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmltemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">home</a>
+                </div>
+                <br/>
+                <h2>
+                    ${heading}
+                </h2>
+                <div>
+                    ${date}
+                </div>
+                
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>`;
+return htmltemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -26,7 +82,7 @@ app.get('/article-three', function(req, res) {
 });
 
 app.get('/article-one', function(req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createtemplate(articleone));
 });
 
 
