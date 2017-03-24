7c4a79ef565232cd59d6c86972fc43d886cd15cf
sandy17031995 <kumarsandeepranjan14@gmail.com>
2017-03-24 14:46:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var articleone = {
              A short story is a piece of prose fiction that can be read in one sitting. Emerging from earlier oral storytellin traditions in the 17th century, the short story has grown to encompass a body of work so diverse as to defy easy characterization. At its most prototypical the short story features a small cast of named characters, and focuses on a self-contained incident with the intent of evoking a "single effect" or mood.[1] In doing so, short stories make use of plot, resonance, and other dynamic components to a far greater degree than is typical of an anecdote, yet to a far lesser degree than a novel. While the short story is largely distinct from the novel, authors of both generally draw from a common pool of literary techniques.
         </p>
         <p>
-            sachin sandeep rahul.
+            sachin sandeep rahul rohit.
         </p>
         <p>
             The short story has been considered both an apprenticeship form preceding more lengthy works, and a crafted form in its own right, collected together in books of similar length, price, and distribution as novels. Short story writers may define their works as part of the artistic and personal expression of the form. They may also attempt to resist categorization by genre and fixed formation.
